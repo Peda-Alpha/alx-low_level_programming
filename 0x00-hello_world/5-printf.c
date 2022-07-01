aad78bc96fda39dbfbb27e83c2ef3f24ce9fc444
Peda-Alpha <peda4sure@gmail.com>
2022-07-01 05:51:16 -0700

    Use the printf function to print text to standard output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
 *
 * Return: Always 0
 */
-int main(void)
+int main(void);
 {
-printf("with proper grammar, but the outcome is a piece of art,, followed by a new line./n")
+printf("with proper grammar, but the outcome is a piece of art,, followed by a new line./n");
+return(0);
 }
