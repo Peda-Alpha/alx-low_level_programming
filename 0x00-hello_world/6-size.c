d95a8ec587d43d026bee2384a0e3e9206d8433c7
Peda-Alpha <peda4sure@gmail.com>
2022-07-01 07:05:37 -0700

    Use the sizeof operator to display the size of data types to standard output

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+  
+printf("Size of char: %d byte(s)", sizeof(a));
+printf("Size of char: %d byte(s)", sizeof(b));
+printf("Size of char: %d byte(s)", sizeof(c));
+printf("Size of char: %d byte(s)", sizeof(d));
+printf("Size of char: %d byte(s)", sizeof(f));
+return (0);
+}
