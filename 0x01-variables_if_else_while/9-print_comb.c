821ef8df04b1137bdf6bc3dd0601f72ad940bb51
Peda-Alpha <peda4sure@gmail.com>
2022-07-03 17:05:42 -0700

    Print all possible combination of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all possible combinations of single digit numbers
+ * Numbers should be printed in ascending order
+ * All code should be in the main function
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+ for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+if (x < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
