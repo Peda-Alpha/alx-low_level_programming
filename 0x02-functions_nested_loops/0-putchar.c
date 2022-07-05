d75bc46230e0b7092941c7196fac6cff096037cc
Peda-Alpha <peda4sure@gmail.com>
2022-07-05 13:34:52 -0700

    Print _putchar using a function _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: Print _putchar followed by a new line
- * Return: 0
- * Author: PEDA ALPHA
+ * main - check the code
+ * Return: Always 0
  */
 int main(void)
 {
-printf("\_putchar\n");
-return (0);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
 }
