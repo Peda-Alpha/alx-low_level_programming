e13ace143e52d0810b4fb75aef94f50f30a0057e
Peda-Alpha <peda4sure@gmail.com>
2022-07-05 07:47:21 -0700

    Create a header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print _putchar followed by a new line
+ * Return: 0
+ * Author: PEDA ALPHA
+ */
+int main(void)
+{
+printf("\_putchar\n");
+return (0);
+}
