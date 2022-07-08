09bea3ab578df17632d2015b457e5d3dfc729faf
Peda-Alpha <peda4sure@gmail.com>
2022-07-08 04:17:38 -0700

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Function prototype
+ * Description: Checks for uppercase
+ * @c: int type character
+ * Return: return 1 else return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
