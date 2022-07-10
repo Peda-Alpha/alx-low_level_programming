c2183bf02297aadb463031c5c862c2fa75dd9f4d
Peda-Alpha <peda4sure@gmail.com>
2022-07-10 11:41:03 -0700

    Fizz Buzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Write a program that prints the numbers from 1 to 100
+ * Followed by a new line
+ * For multiples of three (3), print fizz
+ * For multiples of five (5), print buzz
+ * For numbers which are multiples of both (3) and (5), print fizzbuzz
+ * Each number or word should be separated by a space
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		printf(" ");
+
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			if (x % 3 ==0)
+			printf("Fizz");
+				if (x % 5 == 0)
+				printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
