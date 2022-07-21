94fbf2836a37c77902d820abfea0ff15bd5f3908
Peda-Alpha <peda4sure@gmail.com>
2022-07-21 16:30:55 -0700

    Raise x to the power of y

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ int _putchar(char n);
 int factorial(int n);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
 
 #endif
